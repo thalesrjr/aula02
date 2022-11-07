ba79dc34a0ef097b1484772ba0896325d5ad64d5
thalesrjr5 <thalesrjr5@gmail.com>
2022-11-07 17:24:48 -0300

    loginV2

--- lib/main.dart
+++ lib/main.dart
@@ -7,69 +7,84 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aula02',
       debugShowCheckedModeBanner: false,
+
       theme: ThemeData(
-       
         primarySwatch: Colors.indigo,
+        backgroundColor: Colors.black38,
       ),
-      home: const MyHomePage(),//title: 'Notas'
+      home: const MyHomePage(), //title: 'Notas'
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, });
-
-  
-
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        
-        title: const Text('Notas'),
-        centerTitle: true
-      ),
+      //appBar: AppBar(title: const Text('Notas'), centerTitle: true),
       body: Center(
-      child: Padding (
-        padding: const EdgeInsets.all(32.0),
-    
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-           
-            TextFormField(
-              decoration: const InputDecoration(hintText: "User"),
-            ),
-             const SizedBox(
-              height: 50,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: "Senha"),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Icon(
+                Icons.account_circle,
+                size: 180,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person), hintText: "User"),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    suffixIcon: Icon(Icons.visibility),
+                    hintText: ("Senha")),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 25,
+                    horizontal: 260,
+                  ),
+                  child: Text(
+                    'Login'.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),  ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
